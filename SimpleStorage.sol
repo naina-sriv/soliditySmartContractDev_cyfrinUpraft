47b8c9b3e98b37dc447339bdbc0fa73b3b6138f8
Naina Srivastava <146738328+naina-sriv@users.noreply.github.com>
2025-07-16 21:56:57 +0530

    Create SimpleStorage.sol
    4th lecture

---
+++ SimpleStorage.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+//stating version of solidity as the language is rapidly changing
+// ^ means 0.8.18 and abov will work
+//without ^0.8.18 it means only work with 0.8.18
+//>=0.8.18 <0.9.0 here versions outdie this range wont work
+
+contract SimpleStorage{
+    // Basic data types: uint, boolean, int, address, bytes
+    //uint means unsigned integer
+…}
