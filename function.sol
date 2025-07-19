cae48e8e1758f36ed6dc1c724c3faa3302f3d530
Naina Srivastava <146738328+naina-sriv@users.noreply.github.com>
2025-07-19 22:48:41 +0530

    Create function.sol
    7

---
+++ function.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+contract FuncTest{
+    //uint256 x = 10;
+    uint256 public x;
+    //decoded input	{
+	//"uint256 _x": "98"}
+    
+    //ensure parameter is diff then x
+    function store(uint256 _x) public {
+        x = _x;
+    }
+}
+//use remix vm which is a fake local blockchain
+//no wait
+//fake img accounts with diff values 
+
+//0xd9145CCE52D386f254917e481eB44e9943F39138
+//contract address
+
